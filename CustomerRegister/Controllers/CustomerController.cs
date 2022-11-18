41f18c20dfb0d7bfdd9ba1b5546928ad85694915
André <113135447+andre-pereira-martins@users.noreply.github.com>
2022-11-18 17:08:59 -0300

    Update CustomerRegister/Controllers/CustomerController.cs
    Co-authored-by: Claudinei Santos <103595662+santosclaudinei-warren@users.noreply.github.com>

--- CustomerRegister/Controllers/CustomerController.cs
+++ CustomerRegister/Controllers/CustomerController.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 
 namespace CustomerRegister.Controllers
 {
-    
     [Route("api/[controller]")]
     [ApiController]
     public class CustomerController : ControllerBase
