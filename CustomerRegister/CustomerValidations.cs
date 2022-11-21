2d52ab8fa288022db29868aca5181010d68cab52
André <113135447+andre-pereira-martins@users.noreply.github.com>
2022-11-21 13:28:33 -0300

    Update CustomerRegister/CustomerValidations.cs
    Co-authored-by: Gregory Zimmer <113135420+gregoryzimmerwarren@users.noreply.github.com>

--- CustomerRegister/CustomerValidations.cs
+++ CustomerRegister/CustomerValidations.cs
@@ -50,7 +50,9 @@ namespace CustomerRegister
             int soma;
             int resto;
 
-            if (cpf.Length != 11) return false;
+            if (cpf.Length != 11)
+               return false;
+               
             if (cpf.All(x => x == cpf.First())) return false;
 
             tempCpf = cpf.Substring(0, 9);
