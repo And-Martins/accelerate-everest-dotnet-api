255b356eaf3eabed51af4a4f4851d14fd8f2bd54
André <113135447+andre-pereira-martins@users.noreply.github.com>
2022-11-21 17:18:43 -0300

    Update CustomerRegister/Program.cs
    Co-authored-by: Gregory Zimmer <113135420+gregoryzimmerwarren@users.noreply.github.com>

--- CustomerRegister/Program.cs
+++ CustomerRegister/Program.cs
@@ -5,8 +5,6 @@ using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
-{
    
         var builder = WebApplication.CreateBuilder(args);
 
