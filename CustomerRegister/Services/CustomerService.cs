e662cb66f45b0a6b4867e27df7f3b5c9caace7b6
André <andre.martins@warren.com.br>
2022-11-22 09:57:58 -0300

    refactoring: adding auto increment and removing existId method

--- CustomerRegister/Services/CustomerService.cs
+++ CustomerRegister/Services/CustomerService.cs
@@ -10,12 +10,6 @@ namespace CustomerRegister
     {
         private readonly List<CustomerEntity> _customers = new();
 
-        public void ExistsID(CustomerEntity selectedCustomer)
-        {
-            if (_customers.Any(customer => customer.Id == selectedCustomer.Id))
-                throw new ArgumentException("Este Id já está em uso, por favor escolha outro");
-        }
-
         private void CustomerAlreadyExists(CustomerEntity selectedCustomer)
         {
             if (_customers.Any(customer => customer.Email == selectedCustomer.Email))
@@ -27,8 +21,8 @@ namespace CustomerRegister
 
         public void Add(CustomerEntity customer)
         {
-            ExistsID(customer);
             CustomerAlreadyExists(customer);
+            customer.Id = _customers.LastOrDefault()?.Id + 1 ?? 1;
             _customers.Add(customer);
         }
 
