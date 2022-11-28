5ed5fc91ce02304d49a8e1300bf92eacdfab84cf
André <andre.martins@warren.com.br>
2022-11-28 09:27:23 -0300

    refactor: fix imports

--- AppServices/Services/ICustomerAppService.cs
+++ AppServices/Services/ICustomerAppService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AppServices.Services
+﻿namespace AppServices.Services
 {
     public interface ICustomerAppService
     {
